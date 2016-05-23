b1cc5c241d96f598a8abec02be0c00313232099d
jmeis <jmeis@users.noreply.github.com>
2016-05-23 01:30:40 -0500

    Cleaned up client JavaScript with jQuery

--- template/client/js/main.js
+++ template/client/js/main.js
@@ -1,102 +1,55 @@
-window.onload = function(){
+$(function() {
 
-	var target = document.querySelector('.base-elements__element'),
-			sidebar = document.querySelector('.components__sidebar'),
-			header = document.querySelector('.design-guide__header'),
-			apiConnect = document.querySelector('#primary'),
-			cloudant = document.querySelector('#secondary'),
-			objectStorage = document.querySelector('#floating'),
-			arrow = document.querySelector(".arrow"),
-			about = document.querySelector("#about");
+	// arrow animation
+  $('.arrow').click(function() {
+      $('html, body').animate({
+        scrollTop: $('.base-elements__element').offset().top
+      }, 300);
+      return false;
+  });
 
-	function smooth(element, offset, speed) {
-		if (!(0 >= speed)) {
-			var rate = offset - element.scrollTop,
-					scroll = rate / speed * 10;
-			setTimeout(function() {
-				element.scrollTop = element.scrollTop + scroll, element.scrollTop !== offset && smooth(element, offset, speed - 10)
-			}, 7);
-		}
-	}
+	$(window).scroll(function() {
+		var nav = $('.nav-container');
 
-	arrow.addEventListener("click", function(t) {
-		t.preventDefault(), smooth(document.body, about.offsetTop - 68, 400);
-	});
-
-	window.addEventListener('scroll', function() {
-		var nav = document.querySelector(".nav-container");
-		if (window.scrollY < target.offsetTop) {
-			nav.style.position = "absolute"
-			nav.style.left = 0;
+		// keep sidebar active
+		if (window.scrollY < $('.base-elements__element').offset().top) {
+			nav.css({
+				"position" : "absolute",
+				"left" : 0
+			});
 		}
 		else {
-			nav.style.position = "fixed";
-			nav.style.top = header.clientHeight + "px";
-			nav.style.left = sidebar.offsetLeft + "px";
+			nav.css({
+				"position" : "fixed",
+				"top" : $('.design-guide__header').height() + "px",
+				"left" : $('.components__sidebar').offset().left + "px"
+			});
 		}
+
+		// select buttons on scroll
 		if (window.scrollY < first.offsetTop - 10) {
-			document.getElementById('firstButton').className = ""
-			document.getElementById('secondButton').className = ""
-			document.getElementById('thirdButton').className = ""
-			document.getElementById('fourthButton').className = ""
-			document.getElementById('fifthButton').className = ""
-			document.getElementById('sixthButton').className = ""
+			$('.buttons:not(a.buttons:eq(0))').removeClass("selected-component");
+			$('a.buttons:eq(0)').addClass("selected-component");
 		}
 		else if (window.scrollY < second.offsetTop - 10) {
-			document.getElementById('firstButton').className = "selected-component"
-			document.getElementById('secondButton').className = ""
-			document.getElementById('thirdButton').className = ""
-			document.getElementById('fourthButton').className = ""
-			document.getElementById('fifthButton').className = ""
-			document.getElementById('sixthButton').className = ""
-
+			$('.buttons:not(a.buttons:eq(1))').removeClass("selected-component");
+			$('a.buttons:eq(1)').addClass("selected-component");
 		}
 		else if (window.scrollY < third.offsetTop - 10) {
-			document.getElementById('firstButton').className = ""
-			document.getElementById('secondButton').className = "selected-component"
-			document.getElementById('thirdButton').className = ""
-			document.getElementById('fourthButton').className = ""
-			document.getElementById('fifthButton').className = ""
-			document.getElementById('sixthButton').className = ""
+			$('.buttons:not(a.buttons:eq(2))').removeClass("selected-component");
+			$('a.buttons:eq(2)').addClass("selected-component");
 		}
 		else if (window.scrollY < fourth.offsetTop - 10) {
-			document.getElementById('firstButton').className = ""
-			document.getElementById('secondButton').className = ""
-			document.getElementById('thirdButton').className = "selected-component"
-			document.getElementById('fourthButton').className = ""
-			document.getElementById('fifthButton').className = ""
-			document.getElementById('sixthButton').className = ""
+			$('.buttons:not(a.buttons:eq(3))').removeClass("selected-component");
+			$('a.buttons:eq(3)').addClass("selected-component");
 		}
 		else if (window.scrollY < fifth.offsetTop - 10) {
-			document.getElementById('firstButton').className = ""
-			document.getElementById('secondButton').className = ""
-			document.getElementById('thirdButton').className = ""
-			document.getElementById('fourthButton').className = "selected-component"
-			document.getElementById('fifthButton').className = ""
-			document.getElementById('sixthButton').className = ""
-		}
-		else if (window.scrollY < sixth.offsetTop - 10) {
-			document.getElementById('firstButton').className = ""
-			document.getElementById('secondButton').className = ""
-			document.getElementById('thirdButton').className = ""
-			document.getElementById('fourthButton').className = ""
-			document.getElementById('fifthButton').className = "selected-component"
-			document.getElementById('sixthButton').className = ""
+			$('.buttons:not(a.buttons:eq(4))').removeClass("selected-component");
+			$('a.buttons:eq(4)').addClass("selected-component");
 		}
 		else {
-			document.getElementById('firstButton').className = ""
-			document.getElementById('secondButton').className = ""
-			document.getElementById('thirdButton').className = ""
-			document.getElementById('fourthButton').className = ""
-			document.getElementById('fifthButton').className = ""
-			document.getElementById('sixthButton').className = "selected-component"
+			$('.buttons:not(a.buttons:eq(5))').removeClass("selected-component");
+			$('a.buttons:eq(5)').addClass("selected-component");
 		}
 	});
-
-	window.addEventListener('resize', function() {
-		window.dispatchEvent(new Event('scroll'));
-	})
-
-	window.dispatchEvent(new Event('scroll'));
-	window.dispatchEvent(new Event('resize'));
-}
+});
